486660904d285e824b5b660da20030d1d0125ee9
unknown <iroegbu.dgit@gmail.com>
2024-07-26 12:38:33 +0100

    new

--- src/helpers/redis_helper.ts
+++ src/helpers/redis_helper.ts
@@ -9,12 +9,11 @@ const redis_client = createClient({
   url: String(redis_url),
   socket: {
     reconnectStrategy: (retries) => {
-      // Return the number of milliseconds before reconnecting
       if (retries > 10) {
-        console.log('Too many retries, giving up');
+        console.log('Too many retries, giving up'.red.bold);
         return new Error('Retry limit reached');
       }
-      return Math.min(retries * 100, 3000); // exponential backoff
+      return Math.min(retries * 100, 3000); 
     }
   }
 });
@@ -24,19 +23,19 @@ redis_client.on('error', (err) => {
 });
 
 redis_client.on('connect', () => {
-  console.log('Redis client connected');
+  console.log('Redis client connected'.yellow.bold);
 });
 
 redis_client.on('reconnecting', (retries) => {
-  console.log(`Reconnecting attempt #${retries}`);
+  console.log(`Reconnecting...`);
 });
 
 redis_client.on('end', () => {
-  console.log('Redis client disconnected');
+  console.log('Redis client disconnected'.red.bold);
 });
 
 redis_client.connect().catch((err) => {
-  console.error('Could not establish a connection with Redis', err);
+  console.error('Could not establish a connection with Redis'.red.bold, err);
 });
 
 export default redis_client;
